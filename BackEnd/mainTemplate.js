8146dd7a515acaa2594689073f6a249342c668c5
Nathaniel <nathanielp895@gmail.com>
2022-01-05 18:01:04 -0800

    worked on backend stuff

---
+++ BackEnd/mainTemplate.js
@@ -0,0 +1,14 @@
+/*IMPORTANT - This IS a template with "pre-function", do NOT make changes to this
+file unless creating new "pre-function".*/
+
+/*Retrieves the data from the main index file(s) and creates cookies, contatining
+those values. The "dbHandler" sends the important data to the database by retrieving
+values from the cookis made in the this file.*/
+
+
+function initialize() {
+
+}
+function increaseDecrease() {
+	
+}
